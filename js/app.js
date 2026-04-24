20ab9d9729398a23cb9c97e0ba662f536baa5817
Arseny <terexin-2003@mail.ru>
2026-04-24 18:10:31 +0300

    bug

--- js/app.js
+++ js/app.js
@@ -13,22 +13,22 @@ document.getElementById("userForm").addEventListener("submit", function(event) {
     return;
   }
 
-  if (age == "" || isNaN(age) || age <= 0) {
-    result.textContent = "Ошибка: возраст должен быть числом больше 0";
+  if (age == "" || isNaN(age)) {
+    result.textContent = "Ошибка: заполните поле возраст";
     cleanMessage(result)
     return;
   }
 
   const ageNumber = Number(age);
 
-  if (ageNumber === "" || isNaN(ageNumber) || ageNumber <= 0) {
+  if (ageNumber <= 0) {
     result.textContent = "Ошибка: возраст должен быть числом больше 0";
-  } else if (ageNumber <= 65) {
-    result.textContent = `Привет, ${name}! Доступ разрешен`;
+  } else if (ageNumber >= 65) {
+    result.textContent = `Привет, ${name}! Рекомендуется упрощенный режим`;
   }else if (ageNumber < 18) {
     result.textContent = `Привет, ${name}! Доступ ограничен`;
   }else {
-    result.textContent = `Привет, ${name}! Рекомендуется упрощенный режим`;
+    result.textContent = `Привет, ${name}! Доступ разрешен`;
   }
   cleanMessage(result)
 
